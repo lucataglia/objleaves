686d6f09aaf674c1dc12a5c59fd44f871fb9b291
lucataglia <luca.tagliabue@radicalbit.io>
2021-04-10 16:45:20 +0200

    add test on empty value

--- src/index.test.js
+++ src/index.test.js
@@ -46,3 +46,21 @@ test('objectLeaves must get all leaves', () => {
   const actual = objectLeaves(src);
   expect(actual.sort()).toEqual(expected.sort());
 });
+
+test('objectLeaves must return an empty array', () => {
+  const stringValue = objectLeaves('ciao');
+  const numberValue = objectLeaves(21);
+  const booleanValue = objectLeaves(true);
+  const emptyArray = objectLeaves([]);
+  const emptyObject = objectLeaves({});
+  const nullObject = objectLeaves(null);
+  const undefinedValue = objectLeaves(undefined);
+
+  expect(stringValue).toEqual([]);
+  expect(numberValue).toEqual([]);
+  expect(booleanValue).toEqual([]);
+  expect(emptyArray).toEqual([]);
+  expect(emptyObject).toEqual([]);
+  expect(nullObject).toEqual([]);
+  expect(undefinedValue).toEqual([]);
+});

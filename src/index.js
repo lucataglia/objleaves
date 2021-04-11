9e2963ea3e9e92c2f37fe22f2f49bf57d74fc80a
lucataglia <luca.tagliabue@radicalbit.io>
2021-04-11 23:09:04 +0200

    removed obsolete comment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ export default (obj, acc = []) => {
       return Object.keys(o).forEach((k) => {
         const path = s ? Array.isArray(o) ? `${s}[${k}]` : `${s}.${k}` : k;
 
-        // If it's not a leaf it return an array of trues. So we have to check isLeaf === true.
         const isLeaf = fn(o[k], path) === false;
         if (isLeaf) { acc.push(path); }
       });
